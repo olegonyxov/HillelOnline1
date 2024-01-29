a338f2dfd85e907da8eb2c7c6d3f2876e69fdf9d
olegonyxov <olegonyxov@gmail.com>
2024-01-29 21:47:41 +0200

    5

--- js/app8.js
+++ js/app8.js
@@ -1,5 +1,5 @@
 let a = parseInt(prompt("Input "))
-if (a.slice[0,2] === (a.slice[3,5]).split("").reverse().join("")) {
+if (a.slice[0,3] === (a.slice[2,5]).split("").reverse().join("")) {
     console.log ("зеркальное")
 } else {
     console.log ("не зеркальное")
